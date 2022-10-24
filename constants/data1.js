264b462513b6eceab265e5910eb1d1ee15b92f25
aakrut <aakrutdabhi@gmail.com>
2022-10-24 16:02:10 +0530

    add section comp

---
+++ constants/data1.js
@@ -0,0 +1,22 @@
+export const data1 = [
+  {
+    id: 1,
+    icon: "/svgs/pen-duotone.svg",
+    title: "FLEXIBLE COMPONENTS",
+    content:
+      "No more complicated workflows. Just design and publish. That’s it.",
+  },
+  {
+    id: 2,
+    icon: "/svgs/image-duotone.svg",
+    title: "AMAZING INTEGRATIONS",
+    content:
+      "Grow your business, reach new audiences, and hit your goals with integrations.",
+  },
+  {
+    id: 3,
+    icon: "/svgs/layout-duotone.svg",
+    title: "FULLY RESPONSIVE",
+    content: "Control breakpoints for stunning designs on every device.",
+  },
+];
