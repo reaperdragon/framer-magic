e77511381048d4eeedcf51848765ec09dc030b3d
aakrut <aakrutdabhi@gmail.com>
2022-11-01 11:18:59 +0530

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,25 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Head>
         <title>Framer Magic</title>
-        <link rel="icon" href="/svgs/framer-logo-duotone.svg" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="favicon/site.webmanifest"></link>
 
         <meta
           name="description"
